6c876b19ba5cc3182fa8258af2686064d88fe4ef
Gavin Fielder <gfielder@42.us.org>
2019-04-29 20:21:14 -0700

    adding additional error check to ft_strrepl

--- other/ft_strrepl.c
+++ other/ft_strrepl.c
@@ -6,7 +6,7 @@
 /*   By: gfielder <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/29 19:53:16 by gfielder          #+#    #+#             */
-/*   Updated: 2019/04/29 20:12:27 by gfielder         ###   ########.fr       */
+/*   Updated: 2019/04/29 20:20:52 by gfielder         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 #include "libft.h"
 
 int			ft_strrepl(char **str, unsigned int start, size_t len,
-				char *with)
+				const char *with)
 {
 	size_t	ret_len;
 	size_t	with_len;
@@ -24,7 +24,8 @@ int			ft_strrepl(char **str, unsigned int start, size_t len,
 		return (-1);
 	with_len = ft_strlen(with);
 	ret_len = ft_strlen(*str) + with_len - len;
-	ret = ft_strnew(ret_len);
+	if ((ret = ft_strnew(ret_len)) == NULL)
+		return (-1);
 	ft_memcpy(ret, *str, start);
 	ft_memcpy(ret + start, with, with_len);
 	ft_strcpy(ret + start + with_len, *str + start + len);
